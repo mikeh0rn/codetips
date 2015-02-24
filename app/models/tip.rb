d35caffb368734fa01b99c84be5f773011f2ebdb
Mike Horn <mhorn918@gmail.com>
2015-02-24 00:28:50 -0500

    Add custom query, mapping, search highlighting to Tip model

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -3,5 +3,41 @@ require 'elasticsearch/model'
 class Tip < ActiveRecord::Base
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
+
+  settings index: { number_of_shards: 1 } do
+    mappings dynamic: 'false' do
+      indexes :language, analyzer: 'english', index_options: 'offsets'
+      indexes :topic, analyzer: 'english'
+      indexes :text, analyzer: 'english'
+    end
+  end
+
+  def self.search(query)
+    __elasticsearch__.search(
+      {
+        query: {
+          multi_match: {
+            query: query,
+            fields: ['language^10', 'topic', 'text']
+          }
+        },
+        highlight: {
+          pre_tags: ['<em>'],
+          post_tags: ['</em>'],
+          fields: {
+            language: {},
+            topic: {},
+            text: {}
+          }
+        }
+      }
+    )
+  end
 end
+Tip.__elasticsearch__.client.indices.delete index: Tip.index_name rescue nil
+
+Tip.__elasticsearch__.client.indices.create \
+  index: Tip.index_name,
+  body: { settings: Tip.settings.to_hash, mappings: Tip.mappings.to_hash }
+
 Tip.import
