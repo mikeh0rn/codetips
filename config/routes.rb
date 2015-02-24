5865e8e035f33f38419f20991b0b8cf5c24c4d2b
Mike Horn <mhorn918@gmail.com>
2015-02-24 00:46:40 -0500

    Set root to search view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :tips
-
+  root :to => "search#search"
   get 'search', to: 'search#search'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
