f498e6dba730240317f35de0de26a485f888dcb0
Mike Horn <mhorn918@gmail.com>
2015-02-23 23:30:35 -0500

    Add methods to create, view and list code tips

---
+++ app/controllers/tips_controller.rb
@@ -0,0 +1,26 @@
+class TipsController < ApplicationController
+  def index
+    @tips = Tip.all
+  end
+
+  def show
+    @tip = Tip.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @tip = Tip.new(tip_params)
+    if @tip.save
+      redirect_to @tip
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def tip_params
+      params.require(:tip).permit :title, :text
+    end
+end
